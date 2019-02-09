9894bbb3ea9e9aac9c764d113fe4cdd294062e5f
Arivin Nair <arivin.nair@gmail.com>
2019-02-08 19:46:14 -0800

    Todo List homework final delivery

--- todoCLI/todoCLI.js
+++ todoCLI/todoCLI.js
@@ -1,71 +1,103 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-function welcome() {
+console.log("Welcome to Todo CLI!\n");
+const responseValue = ("(v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit \n  ------------------\n");
+const taskvalue = [];
+
+rl.question(responseValue, (answer) => {
+    start(answer);
+});
+
+// Using regex to capture user input.
+function start(answer) {
+    let cRegex = /[c][0-9]/;
+    let dRegex = /[d][0-9]/;
+
+    switch (true) {
+        case answer === "v":
+            view();
+            rl.question(responseValue, (answer) => {
+                start(answer);
+            });
+            break;
+        case answer === "n":
+            rl.question('Please enter a task: ', (answer) => {
+                addTodo(answer);
+                rl.question(responseValue, (answer) => {
+                    start(answer);
+                });
+            });
+            break;
+        case answer.match(cRegex) !== null:
+            complete(answer);
+            rl.question(responseValue, (answer) => {
+                start(answer);
+            });
+            break;
+        case answer.match(dRegex) !== null:
+            deleteTodo(answer);
+            rl.question(responseValue, (answer) => {
+                start(answer);
+            });
+            break;    
+        case answer === "q":
+            console.log("Goodbye see you soon! ")
+           process.exit();
+            break;
+        default:
+            console.log("Please enter a valid response")
+
+    }
+};
 
 
+function newTask(title) {
+    this.title = title;
+    this.completed = false;
+};
 
-    
-    console.log ("Welcome to Todo CLI!\n",
-    "-------------------\n",
-    
-    "(v) View • (n) New • (cX) Complete • (dX) Delete • (s) Save • (q) Quit");
-    
-    const readline = require('readline');
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+//Function Definition
 
-    });
-
-    let answers= [];
-
-    rl.question('Type a task to add on the command line \n', (answer) => {
-        //answers.push(answer);
-        let answerObj = {"title": answer, "completed" : false  };
-        answers.push(answerObj);
-        
-        console.log(answers[0]);
-        rl.close;
-        process.exit();
+function view() {
+    taskvalue.forEach((task, index) => {
+        let mark = task.completed ? '[✓]' : '[ ]';
+        console.log(`${index}${mark} ${task.title}`)
     });
 }
 
-//Defination
+function addTodo(answer) {
+    // let task = new newTask(answer)  -   this will remove the need for task.title = answer
+    let task = new newTask();
+    task.title = answer;
+    taskvalue.push(task);
+};
 
-    function view() {
+function complete(answer) {
+    let index = answer.split("")[1];
+    if (taskvalue.length < index - 1) {
+        console.log('No task found.');
+    } else {
+        let t = taskvalue[index];
+        t.completed = true;
+        console.log(`${t.title} completed.`)
+    };
+};
 
-    }
-
-    function addTodo() {
-        const readline = require('readline');
-
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-
-        });
-
-        let answers = [];
-        rl.question('Type a task to add on the command line \n', (answer) => {
-            //answers.push(answer);
-            let answerObj = { "title": answer, "completed": false };
-            answers.push(answerObj);
-
-            console.log(answers[0]);
-            rl.close;
-            process.exit();
-        });
-
-    }
-
-    function complete() {
+function deleteTodo(answer) {
+    let index = answer.split("")[1];
+    if (taskvalue.length < index - 1) {
+        console.log('No task found to delete');
+    } else {
+        let del = taskvalue[index];
+        taskvalue.splice(index, 1)
+        console.log(`${del.title} deleted!.`)
 
     }
 
-    function deleteTodo (){
-
-    }
-
-    function quit() {
-        process.exit;
-    }
+ 
+}
